0dfc7800c9036d46357d8c6aeaabb1762a044d72
wowls1227 <53048187+wowls1227@users.noreply.github.com>
2021-07-21 17:30:19 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  type = string
+
 }
 
 variable "prefix" {
   description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
-  type = string
+
 }
 
 variable "name" {
   description = "Your name to attach to the webapp address"
-  type = string
+
 }
